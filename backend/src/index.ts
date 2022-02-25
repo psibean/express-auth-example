b014ae968bf7cdb6a9922954e923a72ba6023e77
psibean <psionatix@gmail.com>
2022-02-25 22:05:37 +1030

    Add EXPRESS_PORT environment variable

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import app from './server/app.js';
+import { EXPRESS_PORT } from './utils/constants.js';
 
 process.on('uncaughtException', error => console.log(error));
 
-app.listen(4444, () => {
-  console.log("Express auth example application is listening at http://localhost:4444...");
+app.listen(EXPRESS_PORT, () => {
+  console.log(`Express auth example application is listening at http://localhost:${EXPRESS_PORT}..`);
 })
