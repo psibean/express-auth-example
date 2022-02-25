68ee2b8a85976590cbf47f7775b4c59b24f4fbc4
psibean <psionatix@gmail.com>
2022-02-26 02:33:35 +1030

    Fix null check and typo

--- backend/src/server/config/session.ts
+++ backend/src/server/config/session.ts
@@ -2,12 +2,12 @@ import session from 'express-session';
 import { v4 as uuidv4 } from 'uuid';
 import { IS_PRODUCTION, SESSION_SECRET } from '../..//utils/constants.js';
 
-const sessionCOnfig = session({
-  secret: SESSION_SECRET,
+const sessionConfig = session({
+  secret: SESSION_SECRET!,
   resave: false,
   saveUninitialized: true,
   cookie: { secure: IS_PRODUCTION, sameSite: 'lax', signed: true },
   genid: () => uuidv4(),
 });
 
-export default sessionCOnfig;
+export default sessionConfig;
