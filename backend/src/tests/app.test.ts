cb5a5557f21a7dbdc60294080e1d7231ba74a97d
psibean <psionatix@gmail.com>
2022-02-26 02:33:47 +1030

    Mock prisma in tests

--- backend/src/tests/app.test.ts
+++ backend/src/tests/app.test.ts
@@ -3,6 +3,16 @@ import supertest from 'supertest';
 
 import app from '../server/app.js';
 
+import { MockContext, Context, createMockContext } from './context'
+
+let mockCtx: MockContext
+let ctx: Context
+
+beforeEach(() => {
+  mockCtx = createMockContext()
+  ctx = mockCtx as unknown as Context
+})
+
 const requester = supertest(app);
 
 it("Successfully loads rooot", async () => {
